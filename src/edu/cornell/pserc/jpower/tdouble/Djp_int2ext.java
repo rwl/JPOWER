dd522a095ee1df423e3cc8204c548d69205c21b0
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-09-15 18:31:29 +0100

    Adding overloaded methods to int2ext.

--- src/edu/cornell/pserc/jpower/tdouble/Djp_int2ext.java
+++ src/edu/cornell/pserc/jpower/tdouble/Djp_int2ext.java
@@ -21,7 +21,15 @@
 
 package edu.cornell.pserc.jpower.tdouble;
 
+import java.lang.reflect.Field;
+
+import cern.colt.matrix.tdouble.DoubleMatrix1D;
 import cern.colt.matrix.tdouble.DoubleMatrix2D;
+import cern.colt.matrix.tint.IntMatrix1D;
+import edu.cornell.pserc.jpower.tdouble.jpc.Djp_areas;
+import edu.cornell.pserc.jpower.tdouble.jpc.Djp_branch;
+import edu.cornell.pserc.jpower.tdouble.jpc.Djp_bus;
+import edu.cornell.pserc.jpower.tdouble.jpc.Djp_gen;
 import edu.cornell.pserc.jpower.tdouble.jpc.Djp_jpc;
 import edu.cornell.pserc.jpower.tdouble.jpc.Djp_order;
 
@@ -34,12 +42,13 @@ import edu.cornell.pserc.jpower.tdouble.jpc.Djp_order;
  */
 public class Djp_int2ext {
 
-	public static DoubleMatrix2D[] jp_int2ext(DoubleMatrix2D bus, DoubleMatrix2D gen,
-			DoubleMatrix2D branch) {
-		return jp_int2ext(bus, gen, branch, null);
+	public static Object[] jp_int2ext(IntMatrix1D i2e, Djp_bus bus,
+			Djp_gen gen, Djp_branch branch) {
+		return jp_int2ext(i2e, bus, gen, branch, null);
 	}
 
 	/**
+	 * old form
 	 *
 	 * @param bus
 	 * @param gen
@@ -47,9 +56,17 @@ public class Djp_int2ext {
 	 * @param areas
 	 * @return
 	 */
-	public static DoubleMatrix2D[] jp_int2ext(DoubleMatrix2D bus,
-			DoubleMatrix2D gen, DoubleMatrix2D branch, DoubleMatrix2D areas) {
-		return null;
+	public static Object[] jp_int2ext(IntMatrix1D i2e, Djp_bus bus,
+			Djp_gen gen, Djp_branch branch, Djp_areas areas) {
+
+		bus.bus_i.assign( i2e.viewSelection(bus.bus_i.toArray()) );
+		gen.gen_bus.assign( i2e.viewSelection(gen.gen_bus.toArray()) );
+		branch.f_bus.assign( i2e.viewSelection(branch.f_bus.toArray()) );
+		branch.t_bus.assign( i2e.viewSelection(branch.t_bus.toArray()) );
+		if (areas != null)
+			areas.price_ref_bus.assign( i2e.viewSelection(areas.price_ref_bus.toArray()) );
+
+		return new Object[] {bus, gen, branch, areas};
 	}
 
 
@@ -108,9 +125,129 @@ public class Djp_int2ext {
 				jpc.areas.update(o.internal.areas, o.areas.status.on);
 
 			/* revert to original bus numbers */
+			jpc.bus.bus_i.viewSelection(o.bus.status.on).assign( o.bus.i2e.viewSelection(jpc.bus.bus_i.viewSelection(o.bus.status.on).toArray()) );
+			jpc.branch.f_bus.viewSelection(o.branch.status.on).assign( o.bus.i2e.viewSelection(jpc.branch.f_bus.viewSelection(o.branch.status.on).toArray()) );
+			jpc.branch.t_bus.viewSelection(o.branch.status.on).assign( o.bus.i2e.viewSelection(jpc.branch.t_bus.viewSelection(o.branch.status.on).toArray()) );
+			jpc.gen.gen_bus.viewSelection(o.gen.status.on).assign( o.bus.i2e.viewSelection(jpc.gen.gen_bus.viewSelection(o.gen.status.on).toArray()) );
+			if (jpc.areas != null)
+				jpc.areas.price_ref_bus.viewSelection(o.areas.status.on).assign( o.bus.i2e.viewSelection(jpc.areas.price_ref_bus.viewSelection(o.areas.status.on).toArray()) );
+			if (o.external != null)
+				o.external = null;
+			o.state = "e";
+			jpc.order = o;
+		} else {
+			System.err.println("int2ext: jpc claims it is already using external numbering.");
 		}
+		return jpc;
+	}
+
+	public static Djp_jpc jp_int2ext(Djp_jpc jpc, String field, String[] ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, field, ordering, dim);
+	}
+
+	public static Djp_jpc jp_int2ext(Djp_jpc jpc, String field, String[] ordering, int dim) {
+		return jp_int2ext(jpc, new String[] {field}, ordering, dim);
+	}
+
+	public static Djp_jpc jp_int2ext(Djp_jpc jpc, String[] field, String[] ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, field, ordering, dim);
+	}
+
+	/**
+	 *
+	 * @param jpc
+	 * @param field
+	 * @param ordering
+	 * @param dim
+	 * @return
+	 */
+	public static Djp_jpc jp_int2ext(Djp_jpc jpc, String[] field, String[] ordering, int dim) {
+		for (String f : field) {
+			try {
+				Field fld = jpc.getClass().getField(f);
+				Class<?> type = fld.getType();
+				if (type == DoubleMatrix1D.class) {
+					DoubleMatrix1D val = (DoubleMatrix1D) fld.get(jpc);
+					fld.set(jpc.order.external, val.copy());
+					fld.set(jpc, jp_int2ext(jpc, val, ordering, dim));
+				} else if (type == DoubleMatrix2D.class) {
+					DoubleMatrix2D val = (DoubleMatrix2D) fld.get(jpc);
+					fld.set(jpc.order.external, val.copy());
+					fld.set(jpc, jp_int2ext(jpc, val, ordering, dim));
+				} else {
+					throw new UnsupportedOperationException();
+				}
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (NoSuchFieldException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return jpc;
+	}
+
+
+	public static DoubleMatrix1D jp_int2ext(Djp_jpc jpc, DoubleMatrix1D val, String ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, val, new String[] {ordering}, dim);
+	}
+
+	public static DoubleMatrix1D jp_int2ext(Djp_jpc jpc, DoubleMatrix1D val, String ordering, int dim) {
+		return jp_int2ext(jpc, val, new String[] {ordering}, dim);
+	}
+
+	public static DoubleMatrix1D jp_int2ext(Djp_jpc jpc, DoubleMatrix1D val, String[] ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, val, ordering, dim);
+	}
+
+	/**
+	 *
+	 * @param jpc
+	 * @param val
+	 * @param ordering
+	 * @param dim
+	 * @return
+	 */
+	@SuppressWarnings("static-access")
+	public static DoubleMatrix1D jp_int2ext(Djp_jpc jpc, DoubleMatrix1D val, String[] ordering, int dim) {
+		Djp_order o = jpc.order;
 
 		return null;
 	}
 
+
+	public static DoubleMatrix2D jp_int2ext(Djp_jpc jpc, DoubleMatrix2D val, String ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, val, new String[] {ordering}, dim);
+	}
+
+	public static DoubleMatrix2D jp_int2ext(Djp_jpc jpc, DoubleMatrix2D val, String ordering, int dim) {
+		return jp_int2ext(jpc, val, new String[] {ordering}, dim);
+	}
+
+	public static DoubleMatrix2D jp_int2ext(Djp_jpc jpc, DoubleMatrix2D val, String[] ordering) {
+		int dim = 1;
+		return jp_int2ext(jpc, val, ordering, dim);
+	}
+
+	/**
+	 *
+	 * @param jpc
+	 * @param val
+	 * @param ordering
+	 * @param dim
+	 * @return
+	 */
+	@SuppressWarnings("static-access")
+	public static DoubleMatrix2D jp_int2ext(Djp_jpc jpc, DoubleMatrix2D val, String[] ordering, int dim) {
+		Djp_order o = jpc.order;
+		return null;
+	}
 }
